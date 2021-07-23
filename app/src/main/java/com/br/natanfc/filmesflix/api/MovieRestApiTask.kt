32e85225a34e2c18e8c006ed782e44649177e072
dijoncavalcante <dijoncavalcante@gmail.com>
2021-07-22 21:57:51 -0400

    feature: Add retrofit object

---
+++ app/src/main/java/com/br/natanfc/filmesflix/api/MovieRestApiTask.kt
@@ -0,0 +1,18 @@
+package com.br.natanfc.filmesflix.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MovieRestApiTask {
+
+    companion object {
+        const val BASE_URL = "https://raw.githubbusercontent.com/"
+    }
+
+    private fun movieProdiver(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun retrofitApi(): MovieApi = movieProdiver().create(MovieApi::class.java)
+}
